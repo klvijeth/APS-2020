8c39a7e826cb666a5bd60c61d67a046ef4b36336
klvijeth <44048623+klvijeth@users.noreply.github.com>
2020-04-27 12:28:16 +0530

    Add files via upload

---
+++ CodeSnippets/dfs_ordering_correctness.py
@@ -0,0 +1,15 @@
+t=int(input())
+for tt in range(t):
+	n=int(input())
+	lx=[]
+	ly=[]
+	flag=0
+	for i in range(n)
+		v,x,y=map(int,input().split())
+		for j in range(len(lx)):
+			if((x>=lx[j] and y>ly[j]) or (x<lx[j] and y>=lx[j])):
+				flag=1
+	if(flag==1):
+		print(1)
+	else:
+		print(0)
